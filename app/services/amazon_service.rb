139569301ea4ae2ce2ff853a4342389fc7724648
Nicolas Jonckheere <nicolasjonckheere22@hotmail.com>
2018-01-15 17:46:52 +0100

    figaro

---
+++ app/services/amazon_service.rb
@@ -0,0 +1,16 @@
+require 'httparty'
+require 'base64'
+
+class AmazonService
+  def self.call_api
+    @full_challenge ||= HTTParty.get('https://e7lqr7l4z5.execute-api.eu-central-1.amazonaws.com/prod/NewCrewMemberChallenge', :headers => {"X-API-key" => ENV["AWS_API_KEY"]})
+    @coded_message ||= @full_challenge.values.first
+    decode_message(@coded_message)
+  end
+
+  private
+
+  def self.decode_message(message)
+    Base64.decode64(message)
+  end
+end
--- config.ru
+++ config.ru
@@ -1,1 +1,6 @@
 require 'sinatra/base'
+
+Dir.glob('./app/{controllers}/*.rb').each { |file| require file }
+
+
+map('/') { run ApplicationController }
